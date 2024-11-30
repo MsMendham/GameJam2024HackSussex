644d9cbf0f4681658dc9620ff670f4ffe241a83f
Jess Williamson <bmw28@sussex.ac.uk>
2024-11-30 22:35:02 +0000

    libfjh

---
+++ Assets/Scripts/PlanetVariables.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlanetVariables : ScriptableObject
+{
+    public Vector2 windSpeed;
+    public float gravity;
+}
